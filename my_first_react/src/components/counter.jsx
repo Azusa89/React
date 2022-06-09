60a645edc6fb08b9865a07f723d9395afc6d0119
Azusa89 <59798103+Azusa89@users.noreply.github.com>
2022-06-09 22:21:12 +0900

    Rendering classes

--- my_first_react/src/components/counter.jsx
+++ my_first_react/src/components/counter.jsx
@@ -1,9 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    render() { 
-        return <h1>Hello World</h1>>;
-    }
+  state = {
+    count: 0,
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClass()}>{this.formatCount()}</span>
+        <button className="btm btn-secondary btn-sm">Increment</button>
+      </div>
+    );
+  }
+  getBadgeClass() {
+    let classes = "badge m-2 bg-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
- 
+
 export default Counter;
--- my_first_react/src/index.js
+++ my_first_react/src/index.js
@@ -3,5 +3,4 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import Counter from './components/counter';
 
-
-ReactDOM.render(<Counter />, document.getElementById('root'))
+ReactDOM.render(<Counter />, document.getElementById('root'));
